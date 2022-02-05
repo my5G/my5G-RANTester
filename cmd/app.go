a0f0b1f5eaccf388ff796c7310780daf234df742
lucas baleeiro <lucasdominato2@gmail.com>
2022-02-05 15:57:25 -0300

    added to cli option for testing AMF response per second

--- cmd/app.go
+++ cmd/app.go
@@ -99,6 +99,38 @@ func main() {
 					return nil
 				},
 			},
+			{
+				Name:    "amf-requests",
+				Aliases: []string{"amf-requests"},
+				Usage: "\nTesting AMF response per second\n" +
+					"Example for testing multiple requests: amf-requests -n 5 \n",
+				Flags: []cli.Flag{
+					&cli.IntFlag{Name: "number-of-requests", Value: 1, Aliases: []string{"n"}},
+				},
+				Action: func(c *cli.Context) error {
+					var numRqs int
+					name := "Testing AMF response per second"
+					cfg := config.Data
+
+					if c.IsSet("number-of-requests") {
+						numRqs = c.Int("number-of-requests")
+					} else {
+						log.Info(c.Command.Usage)
+						return nil
+					}
+
+					log.Info("---------------------------------------")
+					log.Info("[TESTER] Starting test function: ", name)
+					log.Info("[TESTER][UE] Number of Requests per second: ", numRqs)
+					log.Info("[TESTER][GNB] gNodeB control interface IP/Port: ", cfg.GNodeB.ControlIF.Ip, "/", cfg.GNodeB.ControlIF.Port)
+					log.Info("[TESTER][GNB] gNodeB data interface IP/Port: ", cfg.GNodeB.DataIF.Ip, "/", cfg.GNodeB.DataIF.Port)
+					log.Info("[TESTER][AMF] AMF IP/Port: ", cfg.AMF.Ip, "/", cfg.AMF.Port)
+					log.Info("---------------------------------------")
+					// templates.TestMultiUesInQueue(numUes)
+
+					return nil
+				},
+			},
 		},
 	}
 	err := app.Run(os.Args)
