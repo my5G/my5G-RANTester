81a95e725d6c1d8c8e7986bb521949b4cb523a3f
lucas baleeiro <lucasdominato2@gmail.com>
2022-02-23 01:25:43 -0300

    solved problem when sctp socket is closed

--- internal/control_test_engine/gnb/nas/handler/handler.go
+++ internal/control_test_engine/gnb/nas/handler/handler.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"log"
+	log "github.com/sirupsen/logrus"
 	"my5G-RANTester/internal/control_test_engine/gnb/context"
 	"my5G-RANTester/internal/control_test_engine/gnb/ngap/message/ngap_control/nas_transport"
 	"my5G-RANTester/internal/control_test_engine/gnb/ngap/message/sender"
@@ -12,7 +12,8 @@ func HandlerUeInitialized(ue *context.GNBUe, message []byte, gnb *context.GNBCon
 	// encode NAS message in NGAP.
 	ngap, err := nas_transport.SendInitialUeMessage(message, ue, gnb)
 	if err != nil {
-		log.Fatal("[GNB][NGAP] Error making initial UE message: ", err)
+		log.Info("[GNB][NGAP] Error making initial UE message: ", err)
+		return
 	}
 
 	// change state of UE.
@@ -22,7 +23,8 @@ func HandlerUeInitialized(ue *context.GNBUe, message []byte, gnb *context.GNBCon
 	conn := ue.GetSCTP()
 	err = sender.SendToAmF(ngap, conn)
 	if err != nil {
-		log.Fatal("[GNB][AMF] Error sending initial UE message: ", err)
+		log.Info("[GNB][AMF] Error sending initial UE message: ", err)
+		return
 	}
 }
 
@@ -30,14 +32,16 @@ func HandlerUeOngoing(ue *context.GNBUe, message []byte, gnb *context.GNBContext
 
 	ngap, err := nas_transport.SendUplinkNasTransport(message, ue, gnb)
 	if err != nil {
-		log.Fatal("[GNB][NGAP] Error making Uplink Nas Transport: ", err)
+		log.Info("[GNB][NGAP] Error making Uplink Nas Transport: ", err)
+		return
 	}
 
 	// Send Uplink Nas Transport
 	conn := ue.GetSCTP()
 	err = sender.SendToAmF(ngap, conn)
 	if err != nil {
-		log.Fatal("[GNB][AMF] Error sending Uplink Nas Transport: ", err)
+		log.Info("[GNB][AMF] Error sending Uplink Nas Transport: ", err)
+		return
 	}
 }
 
