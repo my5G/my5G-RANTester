e2bed4d3c5f26a14f9b27a115a4210e38619f5a8
AhmedEssam17 <ahmedessam020@gmail.com>
2024-01-31 14:33:32 +0000

    Test Load

--- internal/templates/test-multi-ues-multi-gnbs.go
+++ internal/templates/test-multi-ues-multi-gnbs.go
@@ -38,7 +38,7 @@ func TestMultiUesMultiGNBs(numUes int, numGNBs int) {
 
 	for i := 0; i < numGNBs; i++ {
 		//newGnbID := fmt.Sprintf("%d", gnbID)
-		newGnbID := gnbIdGenerator(gnbID)
+		newGnbID := constructGnbID(gnbID)
 
 		cfg.GNodeB.PlmnList.GnbId = newGnbID
 		cfg.GNodeB.ControlIF.Port = gnbControlPort + i
@@ -60,7 +60,7 @@ func TestMultiUesMultiGNBs(numUes int, numGNBs int) {
 
 		offset := rand.Intn(numGNBs)
 		cfg.GNodeB.ControlIF.Port = gnbControlPort + offset
-		cfg.GNodeB.PlmnList.GnbId = gnbIdGenerator(baseGnbID + offset)
+		cfg.GNodeB.PlmnList.GnbId = constructGnbID(baseGnbID + offset)
 
 		log.Info("Registering ue with gnbControlPort = ", cfg.GNodeB.ControlIF.Port)
 
@@ -80,7 +80,7 @@ func TestMultiUesMultiGNBs(numUes int, numGNBs int) {
 	// log.Info("Total Registeration Time =", executionTime)
 }
 
-func gnbIdGenerator(gnbID int) string {
+func constructGnbID(gnbID int) string {
 	var newGnbID string
 
 	if gnbID <= 9 {
