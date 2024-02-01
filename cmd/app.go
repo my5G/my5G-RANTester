e0b0dc3e1376feb65538d558358f4ebcc78a4019
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-01 11:21:30 +0000

    Test Load

--- cmd/app.go
+++ cmd/app.go
@@ -127,8 +127,8 @@ func main() {
 					// numUes := rand.Intn(5)
 					// numGNBs := rand.Intn(5) + 1
 
-					numUes := 5
-					numGNBs := 2
+					numUes := 7
+					numGNBs := 3
 
 					log.Info("---------------------------------------")
 					log.Info("[TESTER] Starting test function: ", name)
