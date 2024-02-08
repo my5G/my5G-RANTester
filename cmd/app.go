65cfea0644b2def20b366a4fcff6aa49337a738a
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-08 11:37:25 +0000

    Final Touches

--- cmd/app.go
+++ cmd/app.go
@@ -129,8 +129,8 @@ func main() {
 					// numUes := rand.Intn(5)
 					// numGNBs := rand.Intn(5) + 1
 
-					numUes := 100
-					numGNBs := 5
+					numUes := 200
+					numGNBs := 20
 
 					log.Info("---------------------------------------")
 					log.Info("[TESTER] Starting test function: ", name)
