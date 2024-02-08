a5d69a60af792e01c05db7d226651d2f7fb9bcc0
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-08 14:46:56 +0000

    Final Touches

--- internal/control_test_engine/gnb/data/service/service.go
+++ internal/control_test_engine/gnb/data/service/service.go
@@ -85,6 +85,10 @@ func processingData(ue *context.GNBUe, gnb *context.GNBContext, packet []byte) {
 		return
 	}
 
+	log.Info("teidUplink = ", teidUplink)
+	log.Info("packet = ", packet)
+	log.Info("upfAddr = ", upfAddr)
+
 	// send Data plane with GTP header.
 	_, err = conn.WriteToGTP(teidUplink, packet, upfAddr)
 	if err != nil {
