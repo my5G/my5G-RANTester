5cf2a6bcc46975bea82d8f0217545ced7cb7240b
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-09 14:03:21 +0000

    Route

--- cmd/app.go
+++ cmd/app.go
@@ -129,7 +129,7 @@ func main() {
 					// numUes := rand.Intn(5)
 					// numGNBs := rand.Intn(5) + 1
 
-					numUes := 1
+					numUes := 3
 					numGNBs := 1
 
 					log.Info("---------------------------------------")
