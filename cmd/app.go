7c7163f0322b72be98891cb522e5b5180390860c
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-12 08:13:23 +0000

    Ping

--- cmd/app.go
+++ cmd/app.go
@@ -129,7 +129,7 @@ func main() {
 					// numUes := rand.Intn(5)
 					// numGNBs := rand.Intn(5) + 1
 
-					numUes := 250
+					numUes := 260
 					numGNBs := 20
 
 					log.Info("---------------------------------------")
