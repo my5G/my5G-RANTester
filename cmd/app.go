ec43334ce53ae0741bd99a4f1c6b483e10fac076
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-12 12:45:18 +0000

    Load

--- cmd/app.go
+++ cmd/app.go
@@ -129,7 +129,7 @@ func main() {
 					// numUes := rand.Intn(5)
 					// numGNBs := rand.Intn(5) + 1
 
-					numUes := 1050
+					numUes := 1030
 					numGNBs := 20
 
 					log.Info("---------------------------------------")
