bdb392149565c99d5fa4ace6370184a28cc0b4eb
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-19 10:23:15 +0000

    Load

--- internal/control_test_engine/ue/ue.go
+++ internal/control_test_engine/ue/ue.go
@@ -66,60 +66,60 @@ func RegistrationUe(conf config.Config, id uint8, wg *sync.WaitGroup, startTime 
 
 }
 
-func RegistrationUeMonitor(conf config.Config,
-	id uint8, monitor *monitoring.Monitor, wg *sync.WaitGroup, start time.Time) {
+// func RegistrationUeMonitor(conf config.Config,
+// 	id uint8, monitor *monitoring.Monitor, wg *sync.WaitGroup, start time.Time) {
 
-	// new UE instance.
-	ue := &context.UEContext{}
+// 	// new UE instance.
+// 	ue := &context.UEContext{}
 
-	// new UE context
-	ue.NewRanUeContext(
-		conf.Ue.Msin,
-		security.AlgCiphering128NEA0,
-		security.AlgIntegrity128NIA2,
-		conf.Ue.Key,
-		conf.Ue.Opc,
-		"c9e8763286b5b9ffbdf56e1297d0887b",
-		conf.Ue.Amf,
-		conf.Ue.Sqn,
-		conf.Ue.Hplmn.Mcc,
-		conf.Ue.Hplmn.Mnc,
-		conf.Ue.Dnn,
-		int32(conf.Ue.Snssai.Sst),
-		conf.Ue.Snssai.Sd,
-		id,
-		conf.GNodeB.PlmnList.GnbId)
+// 	// new UE context
+// 	ue.NewRanUeContext(
+// 		conf.Ue.Msin,
+// 		security.AlgCiphering128NEA0,
+// 		security.AlgIntegrity128NIA2,
+// 		conf.Ue.Key,
+// 		conf.Ue.Opc,
+// 		"c9e8763286b5b9ffbdf56e1297d0887b",
+// 		conf.Ue.Amf,
+// 		conf.Ue.Sqn,
+// 		conf.Ue.Hplmn.Mcc,
+// 		conf.Ue.Hplmn.Mnc,
+// 		conf.Ue.Dnn,
+// 		int32(conf.Ue.Snssai.Sst),
+// 		conf.Ue.Snssai.Sd,
+// 		id,
+// 		conf.GNodeB.PlmnList.GnbId)
 
-	// starting communication with GNB and listen.
-	err := service.InitConn(ue, start)
-	if err != nil {
-		log.Fatal("Error in", err)
-	} else {
-		log.Info("[UE] UNIX/NAS service is running")
-		// wg.Add(1)
-	}
+// 	// starting communication with GNB and listen.
+// 	err := service.InitConn(ue)
+// 	if err != nil {
+// 		log.Fatal("Error in", err)
+// 	} else {
+// 		log.Info("[UE] UNIX/NAS service is running")
+// 		// wg.Add(1)
+// 	}
 
-	// registration procedure started.
-	trigger.InitRegistration(ue)
+// 	// registration procedure started.
+// 	trigger.InitRegistration(ue)
 
-	for {
+// 	for {
 
-		// UE is register in network
-		if ue.GetStateMM() == 0x03 {
-			elapsed := time.Since(start)
-			monitor.LtRegisterLocal = elapsed.Milliseconds()
-			log.Warn("[TESTER][UE] UE LATENCY IN REGISTRATION: ", monitor.LtRegisterLocal, " ms")
-			break
-		}
+// 		// UE is register in network
+// 		if ue.GetStateMM() == 0x03 {
+// 			elapsed := time.Since(start)
+// 			monitor.LtRegisterLocal = elapsed.Milliseconds()
+// 			log.Warn("[TESTER][UE] UE LATENCY IN REGISTRATION: ", monitor.LtRegisterLocal, " ms")
+// 			break
+// 		}
 
-		// timeout is 10 000 ms
-		if time.Since(start).Milliseconds() >= 10000 {
-			log.Warn("[TESTER][UE] TIME EXPIRED IN UE REGISTRATION 10 000 ms")
-			break
-		}
-	}
+// 		// timeout is 10 000 ms
+// 		if time.Since(start).Milliseconds() >= 10000 {
+// 			log.Warn("[TESTER][UE] TIME EXPIRED IN UE REGISTRATION 10 000 ms")
+// 			break
+// 		}
+// 	}
 
-	wg.Done()
-	// ue.Terminate()
-	// os.Exit(0)
-}
+// 	wg.Done()
+// 	// ue.Terminate()
+// 	// os.Exit(0)
+// }
