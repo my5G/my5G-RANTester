e24f77200ef170252e064ce7fe8f8d99d2a4240a
AhmedEssam17 <ahmedessam020@gmail.com>
2024-02-26 09:49:01 +0000

    Terminate Ue

--- internal/control_test_engine/ue/ue.go
+++ internal/control_test_engine/ue/ue.go
@@ -105,7 +105,8 @@ func RegistrationUeMonitor(conf config.Config,
 		conf.GNodeB.PlmnList.GnbId)
 
 	// starting communication with GNB and listen.
-	err := service.InitConn(ue, ueRegistrationSignal)
+	ueTerminationSignal := make(chan int, 1)
+	err := service.InitConn(ue, ueRegistrationSignal, ueTerminationSignal)
 	if err != nil {
 		log.Fatal("Error in", err)
 	} else {
